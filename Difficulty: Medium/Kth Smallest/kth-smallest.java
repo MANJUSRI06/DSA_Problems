7fa615ce6576fb45eab3fe7e673ed7f9c4c2cb26
Manjusri Shanmugakumar <160816052+MANJUSRI06@users.noreply.github.com>
2026-04-07 22:14:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.java
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -1,6 +1,47 @@
 class Solution {
     public int kthSmallest(int[] arr, int k) {
-        Arrays.sort(arr);
-        return arr[k-1];
+        return quickSelect(arr, 0, arr.length - 1, k - 1);
+    }
+
+    private int quickSelect(int[] arr, int low, int high, int kIndex) {
+        if (low <= high) {
+            int pIndex = partition(arr, low, high);
+
+            if (pIndex == kIndex) {
+                return arr[pIndex];
+            } else if (pIndex > kIndex) {
+                return quickSelect(arr, low, pIndex - 1, kIndex);
+            } else {
+                return quickSelect(arr, pIndex + 1, high, kIndex);
+            }
+        }
+        return -1;
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high && arr[i] <= pivot) {
+                i++;
+            }
+            while (j >= low && arr[j] > pivot) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
     }
 }
